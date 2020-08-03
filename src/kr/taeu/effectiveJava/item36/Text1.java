8ad054caf917852399b96f57267dc02660acbc65
이태우 <twlee@foodtechkorea.com>
2020-08-03 17:08:15 +0900

    item36 수정

--- src/kr/taeu/effectiveJava/item36/Text1.java
+++ src/kr/taeu/effectiveJava/item36/Text1.java
@@ -5,9 +5,9 @@ package kr.taeu.effectiveJava.item36;
  */
 public class Text1 {
   public static final int STYLE_BOLD            = 1 << 0;  // 1
-  public static final int STYLE_ITALIC          = 1 << 0;  // 1
-  public static final int STYLE_UNDERLINE       = 1 << 0;  // 1
-  public static final int STYLE_STRIKETHROUGH   = 1 << 0;  // 1
+  public static final int STYLE_ITALIC          = 1 << 1;  // 2
+  public static final int STYLE_UNDERLINE       = 1 << 2;  // 4
+  public static final int STYLE_STRIKETHROUGH   = 1 << 3;  // 8
   
   // 매개변수 styles는 0개 이상의 STYLE_ 상수를 비트별 OR한 값이다.
   public void applyStyles(int styles) { }
